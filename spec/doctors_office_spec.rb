2e6d4297afbebdfaeb1192fdbb0b5649fb95c980
Dominic Brown and Spencer Ruiz <dominic@>
2017-05-02 10:49:45 -0700

    refactor spec to test the specialty query

--- spec/doctors_office_spec.rb
+++ spec/doctors_office_spec.rb
@@ -5,6 +5,14 @@ require "sequel"
 
 DB = Sequel.connect("postgres://Guest@localhost/hospital_test")
 
+RSpec.configure do |config|
+  config.after(:each) do
+    DB[:patients].delete
+    DB[:doctors].delete
+    DB[:doctor_specialty].delete
+  end
+end
+
 describe "Patient" do
 
   describe ".add" do
@@ -28,8 +36,15 @@ describe "Doctor" do
 
   describe ".add" do
     it "adds a Doctor to the database" do
-      Doctor.add("Dr. Kojisan", "heart surgeon")
-      expect(DB[:doctor_specialty].first[:specialty]).to(eq("heart surgeon"))
+      doctor_id = Doctor.add("Dr. Kojisan", "heart surgeon")
+      query = DB["SELECT specialty FROM doctor_specialty WHERE doctor_id = '#{doctor_id}'"].first[:specialty]
+      expect(query).to(eq("heart surgeon"))
+    end
+  end
+
+  describe ".assign" do
+    it "assigns a doctor to a patient in the database" do
+
     end
   end
 
